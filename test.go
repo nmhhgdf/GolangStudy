9e363aa2a06ab8b11668097f0c9c5cdd739c8d18
Spiral <120575257@qq.com>
2025-12-04 20:59:50 +0800

    first commit

---
+++ test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	time.Sleep(1 * time.Second)
+	fmt.Println("hello Go!")
+}
---
+++ testVar.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int
+	fmt.Println("a = ", a)
+	fmt.Printf("type of a = %T\n", a)
+
+	var b int = 100
+	fmt.Println("b = ", b)
+	fmt.Printf("type of b = %T\n", b)
+
+	var bb string = "abcd"
+	fmt.Printf("bb = %s type of bb = %T\n", bb, bb)
+
+	var c = 100
+	fmt.Println("c = ", c)
+	fmt.Printf("type of c = %T\n", c)
+
+	var cc = "abcd"
+	fmt.Printf("cc = %s, type of cc = %T\n", cc, cc)
+
+	e := 100
+	fmt.Println("e = ", e)
+	fmt.Printf("type of e = %T\n", e)
+
+	f := "abcd"
+	fmt.Println("f = ", f)
+	fmt.Printf("type of f = %T\n", f)
+
+	g := 3.14
+	fmt.Println("g = ", g)
+	fmt.Printf("type of g = %T\n", g)
+
+}
